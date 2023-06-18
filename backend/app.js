446cc100ace766fdcc7150191c0d1302dda53857
Jost Christopher <jostchristopher44@gmail.com>
2023-06-18 06:33:10 +0200

    lalala

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const stuffRoutes = require('./routes/stuff');
+const bookRoutes = require('./routes/book');
 const userRoutes = require('./routes/user');
+const mongoose = require('mongoose');
 const path = require('path');
-
 
 mongoose.connect('mongodb+srv://chris4:chris4Z-@cluster0.rs4wwe4.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -13,18 +12,23 @@ mongoose.connect('mongodb+srv://chris4:chris4Z-@cluster0.rs4wwe4.mongodb.net/?re
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
-
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  );
+  next();
+});
 
-  app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-  app.use('/api/stuff', stuffRoutes);
-  app.use('/api/auth', userRoutes);
-  app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/books', bookRoutes);
+app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
---
+++ backend/routes/book.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+const bookCtrl = require('../controllers/book');
+
+router.get('/', bookCtrl.getAllBook);
+router.post('/', auth, multer, bookCtrl.createBook);
+router.get('/:id', bookCtrl.getOneBook);
+router.put('/:id', auth, multer, bookCtrl.modifyBook);
+router.delete('/:id', auth, bookCtrl.deleteBook);
+
+module.exports = router;
