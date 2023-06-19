12d04784b5e2ab0cd68c8afe8d5d716d8137eb45
Jost Christopher <jostchristopher44@gmail.com>
2023-06-19 19:15:43 +0200

    lala

--- backend/models/books.js
+++ backend/models/books.js
@@ -7,13 +7,6 @@ const bookSchema = mongoose.Schema({
   imageUrl: { type: String, required: true },
   year: { type: Number, required: true },
   genre: { type: String, required: true },
-  ratings: [
-    {
-      userId: { type: String, required: true },
-      grade: { type: Number, required: true },
-    },
-  ],
-  averageRating: { type: Number, required: true },
 });
 
 module.exports = mongoose.model('Book', bookSchema);
